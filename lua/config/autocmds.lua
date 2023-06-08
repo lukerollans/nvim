09ff84e00c4e1ad120001ecdbd02c60510ef06c0
Luke Rollans <luke@rollans.sh>
2023-06-08 20:50:31 +0800

    add some handy autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,1 +1,48 @@
-return {}
+local api = vim.api
+
+-- don't auto comment new line when entering comments
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+-- go to last loc when opening a buffer
+api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = api.nvim_buf_get_mark(0, '"')
+    local lcount = api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- show cursor line only in active window
+local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
+api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  pattern = "*",
+  command = "set cursorline",
+  group = cursorGrp,
+})
+api.nvim_create_autocmd(
+  { "InsertEnter", "WinLeave" },
+  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
+-- enable spell checking for certain file types
+api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  {
+    pattern = { "*.txt", "*.md", "*.tex" },
+    callback = function()
+      vim.opt.spell = true
+      vim.opt.spelllang = "en"
+    end,
+  }
+)
+
+-- change the background color of floating windows and borders.
+api.nvim_create_autocmd('ColorScheme', {
+  callback = function()
+    vim.cmd('highlight NormalFloat guibg=none guifg=none')
+    vim.cmd('highlight FloatBorder guifg=' .. require('config.colours').colors.fg .. ' guibg=none')
+    vim.cmd('highlight NormalNC guibg=none guifg=none')
+  end,
+})
