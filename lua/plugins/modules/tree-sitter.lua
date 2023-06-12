23c9d9976c9518d78e5405d1e95d5f8637a11d95
Luke Rollans <luke@rollans.sh>
2023-06-12 20:48:08 +0800

    only use tree-sitter for elixir atm

--- lua/plugins/modules/tree-sitter.lua
+++ lua/plugins/modules/tree-sitter.lua
@@ -5,21 +5,11 @@ return {
     require('nvim-treesitter.configs').setup {
       auto_install = true,
       highlight = {
-        disable = { 'yaml' },
-        enable = true,
+        enable = { 'elixir' },
         additional_vim_regex_highlighting = false,
       },
       ensure_installed = {
-        'bash',
-        'html',
-        'json',
-        'lua',
-        'markdown',
-        'tsx',
-        'typescript',
         'elixir',
-        'eex',
-        'yaml',
       }
     }
   end
