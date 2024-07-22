14a18fc62791df652ea89394f39b8babc0dfead7
Luke Rollans <luke@rollans.sh>
2024-07-22 21:42:23 +0800

    tweak lazygit style

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,6 +1,5 @@
 -- configure various plugins
 
 -- lazygit
-vim.g.lazygit_floating_window_winblend = 0 -- no transparency on floating window
+vim.g.lazygit_floating_window_winblend = 1 -- no transparency on floating window
 vim.g.lazygit_floating_window_scaling_factor = 0.8 -- make it a little smaller
-vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage the window
