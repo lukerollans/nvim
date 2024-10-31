6d63b7c8e6e03db2aa8df610dce214f63e4e6bc3
Luke Rollans <luke@rollans.sh>
2024-10-31 10:29:39 +0800

    explicitly set dependencies

--- lua/plugins/modules/statusline.lua
+++ lua/plugins/modules/statusline.lua
@@ -3,6 +3,7 @@ return {
   "nvim-lualine/lualine.nvim",
   enabled = true,
   lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   event = { "BufReadPost", "BufNewFile", "VeryLazy" },
   config = function()
     local colors = require('config.colours').colors
