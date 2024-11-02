5b4a79d62906057c4bf5b0f10661e8e75c3a5894
Luke Rollans <luke@rollans.sh>
2024-11-02 15:10:03 +0800

    change cmdline icon to $

--- lua/plugins/modules/cmdline.lua
+++ lua/plugins/modules/cmdline.lua
@@ -2,7 +2,12 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    command_palette = true
+    command_palette = true,
+    cmdline = {
+      format = {
+        cmdline = { icon = "$" }
+      }
+    }
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
