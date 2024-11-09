3c80d9257a8487eb78ccd319471fe52241068f2e
Luke Rollans <luke@rollans.sh>
2024-11-09 16:49:42 +0800

    turn it back

--- lua/config/autocomplete.lua
+++ lua/config/autocomplete.lua
@@ -11,7 +11,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-j>'] = cmp.mapping.complete(),
+    ['<C-k>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
   })
