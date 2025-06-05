3188ff2529ed39db4e9a51113dedbc32d5600d2e
Luke Rollans <luke@rollans.sh>
2025-06-05 17:27:45 +0800

    tweaks

--- lua/plugins/modules/treeview.lua
+++ lua/plugins/modules/treeview.lua
@@ -1,12 +1,15 @@
 return {
   {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+    'stevearc/oil.nvim',
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+      float = {
+        padding = 2,
+      },
+    },
   }
 }
